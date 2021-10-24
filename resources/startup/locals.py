55a40c6baeafc5a112d11d56c30b14025f1bf8f6
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-10-24 11:21:45 +0300

    Update locals.py

--- resources/startup/locals.py
+++ resources/startup/locals.py
@@ -29,7 +29,7 @@ def start():
     print("Welcome to Ultroid, lets start setting up!\n\n")
     print("Cloning the repository...\n\n")
     os.system("rm -rf Ultroid")
-    os.system("git clone https://github.com/TeamUltroid/Ultroid")
+    os.system("git clone https://github.com/Imran95942/ulo")
     print("\n\nDone")
     os.chdir("Ultroid")
     clear_screen()
@@ -66,7 +66,7 @@ def start():
     print(all_done)
     isitdone = input("\n\nIs it all correct? [y/n]")
     if isitdone == "y" or isitdone != "n":
-        # https://github.com/TeamUltroid/Ultroid/blob/31b9eb1f4f8059e0ae66adb74cb6e8174df12eac/resources/startup/locals.py#L35
+        # https://github.com/Imran95942/ulo/blob/31b9eb1f4f8059e0ae66adb74cb6e8174df12eac/resources/startup/locals.py#L35
         f = open(".env", "w")
         f.write(all_done)
     else:
@@ -117,7 +117,7 @@ def check_for_py():
 
 def gen_session():
     print("\nProcessing...")
-    # https://github.com/TeamUltroid/Ultroid/main/resources/startup/locals.py#L35
+    # https://github.com/Imran95942/ulo/main/resources/startup/locals.py#L35
     os.system("python3 resources/session/ssgen.py")
 
 
